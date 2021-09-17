26d815490781d4b876f165c3761b5dfa709f39e7
Cristian Pessanha da Silva <cristian.silva@ccgl.intra>
2021-09-17 13:27:30 -0300

    Inserindo usuarios aula 20

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Controllers\Controller;
+use App\Models\User;
 
 Class AuthController extends Controller
 {
@@ -16,5 +17,18 @@ Class AuthController extends Controller
     {
         if($request->isGet())
         return $this->container->view->render($response, 'register.twig');
+
+        $data = date('d/m/Y');
+        $now = implode("-",array_reverse(explode("/",$data)));
+
+        User::create([
+            'name' => $request->getParam('name'),
+            'email' => $request->getParam('email'),
+            'password' => $request->getParam('password'),
+            'confirmation_key' => 'aaskfpaskf',
+            'confirmation_expires' => $now,
+        ]);
+
+        return $response->withRedirect($this->container->router->pathFor('auth.login'));
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,4 +7,13 @@ use illuminate\Database\Eloquent\Model;
 class User extends Model 
 {
     protected $table = 'users';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'avatar',
+        'confirmation_key',
+        'confirmation_expires'
+    ];
 }
