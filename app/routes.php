d43e93e4bfca1f05483abc88aaa82d13a2f59601
Cristian Pessanha da Silva <cristian.silva@ccgl.intra>
2021-09-17 09:28:33 -0300

    Adicionando links aula 15

--- app/routes.php
+++ app/routes.php
@@ -4,5 +4,10 @@
  *  Primeiro parametro '/' eu indico o caminho para que o usuario possa acessar.
  *  O Segundo paramentro 'HomeController' eu indico que quando o '/' for acessado eu quero que chame esse controller
  *  e ai dentro desse controller eu quero que chame o metodo 'index'.
+ *  setName eu dou o nome da rota, para minha função path_for() funcionar.
  */
-$app->get('/', 'HomeController:index');
+$app->get('/', 'HomeController:index')->setName('home');
+
+//Criando rota de autenticação 
+$app->get('/login', '')->setName('auth.login');
+$app->get('/registrar', '')->setName('auth.register');
