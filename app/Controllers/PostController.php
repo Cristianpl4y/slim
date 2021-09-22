e834735e70d25e37faed367cbc1d2b88925f7c60
Cristian Pessanha da Silva <cristian.silva@ccgl.intra>
2021-09-22 10:48:37 -0300

    Primeiro post aula 38

--- app/Controllers/PostController.php
+++ app/Controllers/PostController.php
@@ -1,12 +1,37 @@
 <?php 
 
 namespace App\Controllers;
+use App\Models\Post;
+use Respect\Validation\Validator as v;
 
 class PostController extends Controller
 {
     public function create($request, $response)
     {
-        return $this->container->view->render($response, 'post/create.twig');
+        if($request->isGet()){
+            return $this->container->view->render($response, 'post/create.twig');
+        }
+
+        $validation = $this->container->validator->validate($request, [
+            'title' => v::length(5)->notEmpty(),
+            'description' => v::notEmpty()
+        ]);
+
+        if($validation->failed()){
+            return $response->withRedirect($this->container->router->pathFor('post.create'));
+        }
+
+        Post::create([
+            'title' => $request->getParam('title'),
+            'description' => $request->getParam('description'),
+            'published' => $request->getParam('published') == '0' ? false: true,
+            'user_id' => $this->container->auth->user()->id
+        ]);
+
+        $this->container->flash->addMessage('success', 'Post foi adicionado com sucesso!');
+        return $response->withRedirect($this->container->router->pathFor('post.create'));
+        
+
     }
    
     public function edit($request, $response)
