ebb79dae92c208e6172af5e1a619f19f98379a2c
cristian <cristianpl4y@gmail.com>
2021-09-21 21:26:16 -0300

    Método Para Salvar Arquivos aula 34

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Controllers;
 
 use App\Controllers\Controller;
 
+use Slim\Http\UploadedFile;
+
 Class UserController extends Controller
 {
     public function avatar($request, $response)
@@ -11,5 +13,16 @@ Class UserController extends Controller
         if($request->isGet()){
             return $this->container->view->render($response, 'avatar.twig');
         }
+    }
+
+    private function moveUploadedFile($directory, UploadedFile $UploadedFile)
+    {
+        $extension = pathinfo($UploadedFile->getClientFilename(), PATHINFO_EXTENSION);
+        $basename = bin2hex(random_bytes(8));
+        $filename = sprintf('%s.%0.8s', $basename, $extension);
+
+        $UploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
+
+        return $filename;
     }
 }
