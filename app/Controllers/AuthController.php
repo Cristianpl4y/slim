58e95aead1caaab8b880307649ae328c970fb3f7
Cristian Pessanha da Silva <cristian.silva@ccgl.intra>
2021-09-29 09:30:26 -0300

    Confirmando E-mails e Resend Function

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -74,4 +74,54 @@ Class AuthController extends Controller
         return $response->withRedirect($this->container->router->pathFor('home'));
       }
     }
+
+    public function confirmation($request, $response)
+    {
+        $user = User::where('confirmation_key', $request->getParam('confirmation'))->first();
+
+        if(!$user){
+            $this->container->flash->addMessage('error', 'A conta que você está tentando confirmar não existe.');
+        }
+       
+        // Ver se o usuario esta dentro do prazo de confirmação 
+        if (strtotime(date('d/m/Y H:i:s')) > strtotime($user->confirmation_expires)) {
+            $this->container->flash->addMessage('error', "Parece que você demorou um pouco para 
+                confirmar o e-mail em? Não tem problema,
+                clique <a href='". $this->container->router->pathFor('auth.resend')."?email=".$user->email."'>aqui</a> para reenviar.");
+        } else {
+            $this->container->flash->addMessage('success', 'Conta confirmada com sucesso!');
+            $user->is_confirmation = true;
+            $user->save();
+        }
+
+        return $response->withRedirect($this->container->router->pathFor('auth.login'));
+    }
+
+    public function resend($request, $response)
+    {
+        if(empty($request->getParam('email'))) {
+            $this->container->flash->addMessage('error', 'Houve um erro ao tentar processar a sua solicitação.');
+            return $response->withRedirect($this->container->router->pathFor('auth.login'));
+        }
+
+        $now = new \Datetime(date('d/m/Y H:i:s'));
+        $now->modify('+1 hour');
+        $key = bin2hex(random_bytes(20));
+
+        $user = User::where('email', $request->getParam('email'))->first();
+        $user->confirmation_key = $key;
+        $user->confirmation_expires = $now;
+        $user->save();
+
+        $payload = [
+            'name' => $user->name,
+            'email' => $user->email,
+            'confirmation' => $key,
+        ];
+
+        $this->container->mail->send($payload, 'resend.twig', 'Reenvio de confirmação!', $payload);
+
+        $this->container->flash->addMessage('success', 'Um novo e-mail de confirmação foi enviado com sucesso!');
+        return $response->withRedirect($this->container->router->pathFor('auth.login'));
+    }
 }
--- app/routes.php
+++ app/routes.php
@@ -19,6 +19,8 @@ $app->group('/auth', function($app){
     // GET E POST
     $app->map(['GET', 'POST'],'/login', 'AuthController:login')->setName('auth.login');
     $app->map(['GET', 'POST'],'/registrar', 'AuthController:register')->setName('auth.register');
+    $app->get('/confirmacao', 'AuthController:confirmation');
+    $app->get('/reenviar', 'AuthController:resend')->setName('auth.resend');
     $app->get('/logout', 'AuthController:logout')->setName('auth.logout');
 });
 
